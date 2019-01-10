2792e6031ddd9384a43d7670043e992f70059154
Obed Parlapiano <obed.marquezp@gmail.com>
2019-01-10 21:54:01 +0100

    Remove Proudly by Wordpress. Add link to theme github

--- footer.php
+++ footer.php
@@ -17,10 +17,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
             <?php get_sidebar( 'footer' ); ?>
-		<div class="site-info">      
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'life-notes' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'life-notes' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'life-notes' ), 'Life Notes', '<a href="https://twitter.com/ObedParla" rel="designer">Obed M\' Parlapiano</a>' ); ?>
+		<div class="site-info">
+			Theme: <a href="https://github.com/obedparla/life-notes" rel="theme">Life Notes</a> by
+			<a href="https://twitter.com/ObedParla" rel="designer">Obed Marquez Parlapiano</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
